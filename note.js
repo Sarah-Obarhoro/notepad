fa87ac1a25e0484bbf989919cd6b94bce227eefa
Sarah Obarhoro <obarhorosarah@gmail.com>
2022-10-26 16:24:56 +0100

    Updated codes

--- note.js
+++ note.js
@@ -1,11 +1,10 @@
 
 let note = document.querySelector("[name=notes]");
-let newNote = document.querySelector(".mynotes")
+let content = document.querySelector(".mynotes")
 let saveNote = document.querySelector("[done]")
 let entry = document.querySelector("#mynotes")
 let msg = document.querySelector("#msg")
 let form = document.querySelector("#form");
-
 
 //focus on text area
 document.querySelector("[name=add]").addEventListener("click", (e) => {
@@ -21,7 +20,9 @@ form.addEventListener("submit", function (e) {
         return;
     
     acceptUserEntry();
-    createNote();
+
+    //rebuild notes with latest saved changes
+    buildNotes();
 })
 
 let validate = () => {
@@ -34,22 +35,110 @@ let validate = () => {
     return validated;
 }
 
-let userEntry = [{}];
-
 let acceptUserEntry = () => {
-    userEntry.push (note.value);
+    let notes = JSON.parse(localStorage.getItem("userEntry")) ?? []
+    notes.push({
+        id: Math.floor(Math.random() * 99999),
+        note: note.value
+    })
 
-    localStorage.setItem("userEntry", JSON.stringify(userEntry));
-
-    console.log(userEntry)
-}
-
-let createNote = () => {
-    const list = note.value;
+    localStorage.setItem("userEntry", JSON.stringify(notes));
     note.value = '';
 
-    const myList = document.createElement('li');
-    myList.innerText = list;
-    newNote.append(myList);
-    
+    // console.log(userEntry)
 }
+
+let buildNotes = () => {
+    const data = JSON.parse(localStorage.getItem('userEntry')) ?? {};
+    const notes = Object.values(data)
+
+    content.innerHTML = "";
+
+    notes.forEach((data) => {
+        
+        const newList = document.createElement('li');
+        newList.setAttribute('data-note-id', data.id);
+        newList.innerHTML = `
+            ${data['note']} 
+            <div class="editnotes">
+                <button name="edit" type="button" data-note-id="${data.id}"> <i class="fa fa-edit"></i> </button>
+                <button name="delete" type="button" data-note-id="${data.id}"> <i class="fa fa-trash"></i> </button>
+            </div>`;
+    
+            content.appendChild(newList);    
+
+    });
+
+    deleteEvent();
+    editNote();
+}
+
+let editNote = () => {
+    let edit = document.querySelectorAll('[name=edit]')
+
+    edit.forEach((e) => {
+        e.addEventListener('click', (e) => {
+            let button = e.target
+            let noteId = button.getAttribute('data-note-id')
+
+            const data = JSON.parse(localStorage.getItem('userEntry')) ?? {};
+            let notes = Object.values(data)
+
+            let getNote = data.filter ((note) => {
+                return note.id == noteId
+            })[0]
+
+            note.value = getNote.note
+            note.focus()
+        })            
+    })
+
+
+
+}
+
+let deleteEvent = () => {
+    let del = document.querySelectorAll('[name=delete]')
+
+    del.forEach(function(e){
+        e.addEventListener('click', (e) => {
+            let button = e.target
+            let noteId = button.getAttribute('data-note-id')
+
+            //get notes from data storage
+            const data = JSON.parse(localStorage.getItem('userEntry')) ?? {};
+            let notes = Object.values(data)
+
+            // notes.map((data, index) => {
+
+            //     if(data.id == noteId){
+            //         notes.splice(index, 1);
+            //         localStorage.setItem("userEntry", JSON.stringify(notes));
+            //         buildNotes();
+            //     }
+            // })
+
+            notes = notes.filter((data) => {
+                return data.id != noteId
+            });
+
+            localStorage.setItem("userEntry", JSON.stringify(notes));
+            buildNotes();
+
+
+            console.log(notes)
+
+            // console.log(noteId)
+        });
+    })
+}
+
+// build notes once page loads
+buildNotes();
+
+// setInterval(() => {
+//     buildNotes();
+
+//     // console.log('Notes listener Fired!')
+
+// }, 1000);
